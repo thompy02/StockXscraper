95e52c7710d41efb89fb103fca553b274507fb5e
Trace Thompson <thompsontrace13@gmail.com>
2021-09-26 12:11:34 -0700

    StockXscraper.py

--- StockXscraper.py
+++ StockXscraper.py
@@ -1,12 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
 
+URL = "https://stockx.com/air-jordan-5-retro-moonlight-2021"
+page = requests.get(URL)
+
+print(page.text)
+
 source = requests.get('https://stockx.com/air-jordan-5-retro-moonlight-2021').text
 soup = BeautifulSoup(source, 'lxml')
 
 ##BuyPrice = soup.find('div', class_='<div class="chakra-stat css-1mbo1ls">')
 
 ShoeName = soup.find("div",'class="chakra-heading css-1gbu8yz')
-print(ShoeName.prettify())
+print(ShoeName.text.prettify())
 
 ##ShoeSku = soup.find
