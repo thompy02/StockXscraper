22b5ae6126dd6e6f911a3a8fb3964338336bfde3
Trace Thompson <thompsontrace13@gmail.com>
2021-10-02 13:26:03 -0700

    StockXscraper.py

--- StockXscraper.py
+++ StockXscraper.py
@@ -2,14 +2,25 @@ from selenium import webdriver
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
-driver.get("https://stockx.com/air-jordan-5-retro-moonlight-2021")
+##"https://stockx.com/air-jordan-5-retro-moonlight-2021"
+
+ShoeLink = input("paste your shoe link here")
+
+driver.get(ShoeLink)
 
 LastSale = driver.find_element_by_xpath('//*[@id="main-content"]/div/section[1]/div[3]/div[2]/div[2]/div[1]/p[2]')
 
-print(LastSale)
+print(LastSale.text + " Is the Price of the last shoe sold")
 
+BuyPrice = driver.find_element_by_xpath('//*[@id="main-content"]/div/section[1]/div[3]/div[2]/div[1]/div[2]/div[1]/div/dl/dd')
 
-##BuyPrice = 
-
+print(BuyPrice.text + " Is the current Price to buy")
 
 ##ShoeSku = 
+
+PriceFormula = 1.2
+
+PriceAdjustment = (float(LastSale.text) * float(PriceFormula))
+
+print(PriceAdjustment)
+
